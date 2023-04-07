e9273d1cf6ab2e72d3da0997d305de083a83413b
itsjonny96 <122654221+itsjonny96@users.noreply.github.com>
2023-04-07 14:57:01 -0400

    Updated nozzle home offset and probe offset
    Adjusted the nozzle x and y min position to compensate for custom carriage and center the nozzle.
    Adjusted the probe offset from the nozzle.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1561,7 +1561,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 27.25, -12.8, -2 }
+#define NOZZLE_TO_PROBE_OFFSET { 31.4, -9.1, -2 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
@@ -1782,8 +1782,8 @@
 #define Y_BED_SIZE 300
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -2
-#define Y_MIN_POS -5
+#define X_MIN_POS -7.8
+#define Y_MIN_POS -5.7
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
